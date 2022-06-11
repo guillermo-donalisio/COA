525a2886ddc393d96d30ff200990b384b560dc52
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-06-11 14:20:30 -0300

    UserController

---
+++ COA_Api/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace COA_Api.Controllers;
+
+[ApiController]
+public class UserController : ControllerBase
+{
+    // [HttpGet]
+    // [Route("get/users")]
+    // public IEnumerable<WeatherForecast> Get()
+    // {
+    //     return Ok();
+    // }
+}
